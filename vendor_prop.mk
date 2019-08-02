f78f6a9723941230acfa7f44d373aebc145d84c4
sunnyraj84348 <sunnyraj84348@gmail.com>
2019-08-03 00:41:06 +0530

    Tweaks For Better Ram Management
    Signed-off-by: Asad <Asadali68700@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -76,6 +76,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio_hal.in_period_size=144 \
     vendor.audio_hal.period_multiplier=3 \
     vendor.audio.adm.buffering.ms=2
+#Tweaks
+ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.hw.binder.size_kbyte=1024
